d9a92edee6a6759fe52343f9206cb58adace19a8
Sergey <20087169+Xitcklif@users.noreply.github.com>
2022-01-17 23:31:05 +0300

    17.01.22.23.31

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,7 +1,9 @@
 package app.config;
 
+import app.model.Timer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 @ComponentScan(basePackages = "app")
